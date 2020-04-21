a1c49e335352803f33c279b723652ea79a6e0b30
smicle <bhhs1270026@gn.iwasaki.ac.jp>
2020-04-21 22:56:26 +0900

    Add dist

---
+++ dist/index.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Get environment variables.
+ * If it does not exist, output an error and exit.
+ * @param key Environment variable name.
+ */
+export declare const throwEnv: (key: string) => string;
---
+++ dist/index.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+exports.__esModule = true;
+var dotenv = __importStar(require("dotenv"));
+dotenv.config();
+exports.throwEnv = function (key) {
+    var val = process.env[key];
+    if (!val)
+        throw new Error(key + " is not in environment variable.");
+    return val;
+};
