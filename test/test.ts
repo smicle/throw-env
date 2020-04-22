731cc86e57e52695cd4138640c1cbd666bbe754d
smicle <bhhs1270026@gn.iwasaki.ac.jp>
2020-04-22 16:49:11 +0900

    Add testの追加

---
+++ test/test.ts
@@ -0,0 +1,12 @@
+import throwEnv from '../src'
+
+describe('Get the value of an environment variable', () => {
+  test('I was able to get the value', () => {
+    expect(throwEnv('test')).toBe('test')
+  })
+  test('Could not get value', () => {
+    const key = 'value'
+    const err = new Error(`${key} is not in environment variable.`)
+    expect(() => throwEnv(key)).toThrowError(err)
+  })
+})
